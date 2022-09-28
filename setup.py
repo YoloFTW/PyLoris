dbefdd96e3a414ea3e23957931f40c8abb293492
Andre <andretoddriddle@hotmail.com>
2022-09-28 16:26:51 +0100

    cleaning branch

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '1.0.1'
+VERSION = '1.0.2'
 DESCRIPTION = 'Test websites for slow loris'
 LONG_DESCRIPTION = long_description
 
@@ -18,6 +18,7 @@ setup(
     version=VERSION,
     author="YoloFTW",
     description=DESCRIPTION,
+    url='https://github.com/YoloFTW/PyLoris'
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
